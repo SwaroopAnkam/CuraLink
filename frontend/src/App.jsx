a34dc0430ce740fd15b2e17e25c8402dd93944cc
Swaroop Ankam <lcb2022035@iiitl.ac.in>
2025-02-12 22:51:19 +0530

    Initilized Project Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className='bg-cyan-700 text-center align-middle h-screen flex justify-center items-center text-green-500 text-9xl'>
+      CuraLink
+    </div>
+  )
+}
+
+export default App
